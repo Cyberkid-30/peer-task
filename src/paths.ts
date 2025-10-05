26066c1f57b3d14e8486ff1b1e89527dbf8648e6
Cyberkid-30 <janphandoh@gmail.com>
2025-10-05 14:28:43 +0000

    Update path references from "tickets" to "tasks"

--- src/paths.ts
+++ src/paths.ts
@@ -7,6 +7,6 @@ export const passwordForgotPath = () => "/password-forgot";
 export const accountProfilePath = () => "/account/profile";
 export const accountPasswordPath = () => "/account/password";
 
-export const ticketsPath = () => "/tickets";
-export const ticketPath = (ticketId: string) => `/tickets/${ticketId}`;
-export const ticketEditPath = (ticketId: string) => `/tickets/${ticketId}/edit`;
+export const ticketsPath = () => "/tasks";
+export const ticketPath = (ticketId: string) => `/tasks/${ticketId}`;
+export const ticketEditPath = (ticketId: string) => `/tasks/${ticketId}/edit`;
